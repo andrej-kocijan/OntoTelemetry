f7e7b893bc765f00f5d05bc44da82700da894a4e
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-10-17 11:55:34 +0200

    Implemented loading the ontology into fuseki

--- src/main/java/si/fri/liis/FusekiServerConfig.java
+++ src/main/java/si/fri/liis/FusekiServerConfig.java
@@ -1,39 +1,90 @@
 package si.fri.liis;
 
 import org.apache.jena.fuseki.main.FusekiServer;
+import org.apache.jena.ontology.OntModel;
 import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionRemote;
+import org.apache.jena.reasoner.Reasoner;
+import org.apache.jena.reasoner.ReasonerRegistry;
 import org.apache.jena.tdb2.TDB2Factory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import si.fri.liis.Helpers.QueryHelpers;
+
+import java.io.InputStream;
+import java.net.URL;
 
 @Component
 public class FusekiServerConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(FusekiServerConfig.class);
+    private static final String ONTOLOGY_URL = "https://raw.githubusercontent.com/andrej-kocijan/opentelemetry-ontology/refs/heads/main/OpenTelemetry.ttl";
+
     @Bean
     public CommandLineRunner runFusekiServer() {
         return args -> {
-            // Create a dataset (e.g., using TDB2 or in-memory for testing)
+
             Dataset dataset = TDB2Factory.connectDataset("data");
 
-            // Optionally, load an OWL ontology
-//            OntModel ontModel = ModelFactory.createOntologyModel();
-//            ontModel.read("ontology.owl"); // Load your OWL ontology here
-
-            // Add reasoning capabilities (optional)
-//            Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-//            Model infModel = ModelFactory.createInfModel(reasoner, ontModel);
-//            dataset.getDefaultModel().add(infModel);
-
-            // Start Fuseki server
             FusekiServer server = FusekiServer
                     .create()
-                    .port(3030)  // Choose the port for Fuseki
-                    .add("/ds", dataset)  // Dataset name and Fuseki dataset
+                    .port(3030)
+                    .add("/ds", dataset)
                     .build();
 
-            System.out.println("Starting Fuseki server...");
+            logger.info("Starting Fuseki server...");
             server.start();
+
+            if(ontologyLoaded())
+                logger.info("Ontology already loaded...");
+            else
+                loadOntology();
         };
+    }
+
+    private boolean ontologyLoaded() {
+
+        try (RDFConnection conn = RDFConnectionRemote.service("http://localhost:3030/ds").build()){
+            Query q = QueryHelpers.createQuery("""
+                    ASK { <http://www.semanticweb.org/andrej/ontologies/2024/9/opentelemetry-ontology> rdf:type owl:Ontology . }
+                    """);
+
+            return conn.queryAsk(q);
+
+        } catch (Exception e) {
+            logger.error("Error while checking for ontology: {}", e.getMessage());
+        }
+
+        return false;
+    }
+
+    private void loadOntology() {
+
+        logger.info("Loading ontology...");
+        try (RDFConnection conn = RDFConnectionRemote.service("http://localhost:3030/ds").build()) {
+
+            OntModel ontModel = ModelFactory.createOntologyModel();
+
+            try (InputStream in = new URL(ONTOLOGY_URL).openStream()) {
+                ontModel.read(in, null, "TTL");
+            } catch (Exception e) {
+                logger.error("Error while reading ontology: {}", e.getMessage());
+            }
+
+            Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
+            Model infModel = ModelFactory.createInfModel(reasoner, ontModel);
+
+            conn.load(infModel);
+            logger.info("Ontology loaded successfully into Fuseki.");
+        } catch (Exception e) {
+            logger.error("Could not load ontology: {}", e.getMessage());
+        }
     }
 }
---
+++ src/main/java/si/fri/liis/Helpers/QueryHelpers.java
@@ -0,0 +1,36 @@
+package si.fri.liis.Helpers;
+
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.shared.PrefixMapping;
+
+public class QueryHelpers {
+
+    private static PrefixMapping getPrefixes() {
+
+        return PrefixMapping.Factory.create()
+                .setNsPrefix("", "http://www.semanticweb.org/andrej/ontologies/2024/9/opentelemetry-ontology/")
+                .setNsPrefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
+                .setNsPrefix("owl", "http://www.w3.org/2002/07/owl#")
+                .setNsPrefix("xsd", "http://www.w3.org/2001/XMLSchema#")
+                .setNsPrefix("rdfs", "http://www.w3.org/2000/01/rdf-schema#");
+    }
+
+    public static Query createQuery(String query) {
+
+        StringBuilder sb = new StringBuilder();
+        PrefixMapping prefixes = getPrefixes();
+
+        for(String prefix: prefixes.getNsPrefixMap().keySet()) {
+            sb.append("PREFIX ");
+            sb.append(prefix);
+            sb.append(": <");
+            sb.append(prefixes.getNsPrefixURI(prefix));
+            sb.append(">\n");
+        }
+
+        sb.append(query);
+
+        return QueryFactory.create(sb.toString());
+    }
+}
