792a2deeb5fb7576835cf062f052d1988e0c8c6a
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-10-18 18:44:20 +0200

    Create a ttl config for fuseki

--- src/main/java/si/fri/liis/FusekiServerConfig.java
+++ src/main/java/si/fri/liis/FusekiServerConfig.java
@@ -1,25 +1,21 @@
 package si.fri.liis;
 
 import org.apache.jena.fuseki.main.FusekiServer;
-import org.apache.jena.ontology.OntModel;
-import org.apache.jena.query.Dataset;
 import org.apache.jena.query.Query;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdfconnection.RDFConnection;
+import org.apache.jena.rdfconnection.RDFConnectionFuseki;
 import org.apache.jena.rdfconnection.RDFConnectionRemote;
-import org.apache.jena.reasoner.Reasoner;
-import org.apache.jena.reasoner.ReasonerRegistry;
-import org.apache.jena.tdb2.TDB2Factory;
+import org.apache.jena.rdfconnection.RDFConnectionRemoteBuilder;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFLanguages;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import si.fri.liis.Helpers.QueryHelpers;
-
-import java.io.InputStream;
-import java.net.URL;
 
 @Component
 public class FusekiServerConfig {
@@ -31,18 +27,16 @@ public class FusekiServerConfig {
     public CommandLineRunner runFusekiServer() {
         return args -> {
 
-            Dataset dataset = TDB2Factory.connectDataset("data");
-
             FusekiServer server = FusekiServer
                     .create()
                     .port(3030)
-                    .add("/ds", dataset)
+                    .parseConfigFile("src/main/resources/fuseki.config.ttl")
                     .build();
 
             logger.info("Starting Fuseki server...");
             server.start();
 
-            if(ontologyLoaded())
+            if (ontologyLoaded())
                 logger.info("Ontology already loaded...");
             else
                 loadOntology();
@@ -51,7 +45,10 @@ public class FusekiServerConfig {
 
     private boolean ontologyLoaded() {
 
-        try (RDFConnection conn = RDFConnectionRemote.service("http://localhost:3030/ds").build()){
+        RDFConnectionRemoteBuilder connBuilder = RDFConnectionFuseki
+                .service("http://localhost:3030/ds/query");
+
+        try (RDFConnection conn = connBuilder.build()) {
             Query q = QueryHelpers.createQuery("""
                     ASK { <http://www.semanticweb.org/andrej/ontologies/2024/9/opentelemetry-ontology> rdf:type owl:Ontology . }
                     """);
@@ -68,20 +65,17 @@ public class FusekiServerConfig {
     private void loadOntology() {
 
         logger.info("Loading ontology...");
-        try (RDFConnection conn = RDFConnectionRemote.service("http://localhost:3030/ds").build()) {
+        try (RDFConnection conn = RDFConnectionRemote.service("http://localhost:3030/ds/load").build()) {
 
-            OntModel ontModel = ModelFactory.createOntologyModel();
+            Model model = ModelFactory.createDefaultModel();
 
-            try (InputStream in = new URL(ONTOLOGY_URL).openStream()) {
-                ontModel.read(in, null, "TTL");
+            try {
+                RDFDataMgr.read(model, ONTOLOGY_URL, RDFLanguages.TTL);
             } catch (Exception e) {
                 logger.error("Error while reading ontology: {}", e.getMessage());
             }
 
-            Reasoner reasoner = ReasonerRegistry.getOWLReasoner();
-            Model infModel = ModelFactory.createInfModel(reasoner, ontModel);
-
-            conn.load(infModel);
+            conn.load(model);
             logger.info("Ontology loaded successfully into Fuseki.");
         } catch (Exception e) {
             logger.error("Could not load ontology: {}", e.getMessage());
