dec24201567d16d9c7bb44273ea31c64d218ec1f
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-10-19 19:29:36 +0200

    Started work on scope spans converter

--- src/main/java/si/fri/liis/Converters/TraceConverter.java
+++ src/main/java/si/fri/liis/Converters/TraceConverter.java
@@ -1,10 +1,12 @@
 package si.fri.liis.Converters;
 
 import io.opentelemetry.proto.trace.v1.ResourceSpans;
+import io.opentelemetry.proto.trace.v1.ScopeSpans;
 import io.opentelemetry.proto.trace.v1.TracesData;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.vocabulary.RDF;
+import si.fri.liis.Converters.Common.InstrumentationScopeConverter;
 import si.fri.liis.Converters.Common.ResourceConverter;
 
 import java.util.ArrayList;
@@ -22,10 +24,10 @@ public class TraceConverter extends Converter<TracesData> {
 
         Resource td = model.createResource(ontoUri + "tracesData" + UUID.randomUUID());
 
-        Property tracesDataProperty = model.createProperty(ontoUri + "TracesData");
+        Property tracesDataProperty = model.createProperty(ontoUri, "TracesData");
         td.addProperty(RDF.type, tracesDataProperty);
 
-        Property resourceSpanProperty = model.createProperty(ontoUri + "resourceSpan");
+        Property resourceSpanProperty = model.createProperty(ontoUri, "resourceSpan");
 
         ArrayList<Resource> resourceSpansResources = convertResourceSpans();
 
@@ -39,10 +41,10 @@ public class TraceConverter extends Converter<TracesData> {
         List<ResourceSpans> resourceSpansList = source.getResourceSpansList();
         ArrayList<Resource> resources = new ArrayList<>();
 
-        Property resourceSpanProperty = model.createProperty(ontoUri + "ResourceSpans");
-        Property schemaUrlProperty = model.createProperty(ontoUri + "schemaUrl");
-        Property resourceProperty = model.createProperty(ontoUri + "resource");
-        Property scopeSpanProperty = model.createProperty(ontoUri + "scopeSpan");
+        Property resourceSpanProperty = model.createProperty(ontoUri, "ResourceSpans");
+        Property schemaUrlProperty = model.createProperty(ontoUri, "schemaUrl");
+        Property resourceProperty = model.createProperty(ontoUri, "resource");
+        Property scopeSpanProperty = model.createProperty(ontoUri, "scopeSpan");
 
         for(ResourceSpans resourceSpans : resourceSpansList) {
 
@@ -53,9 +55,33 @@ public class TraceConverter extends Converter<TracesData> {
             Resource resourceResource = (new ResourceConverter(model, resourceSpans.getResource())).getConvertedResource();
             resourceSpansResource.addProperty(resourceProperty, resourceResource);
 
+            for(ScopeSpans scopeSpans : resourceSpans.getScopeSpansList()) {
+                Resource scopeSpanResource = convertScopeSpans(scopeSpans);
+                resourceSpansResource.addProperty(scopeSpanProperty, scopeSpanResource);
+            }
+
             resources.add(resourceSpansResource);
         }
 
         return resources;
+    }
+
+    private Resource convertScopeSpans(ScopeSpans scopeSpans) {
+
+        Resource resource = model.createResource(ontoUri + "scopeSpans" + UUID.randomUUID());
+        Property scopeSpansProperty = model.createProperty(ontoUri, "ScopeSpans");
+        resource.addProperty(RDF.type, scopeSpansProperty);
+
+        Property schemaUrlProperty = model.createProperty(ontoUri, "schemaUrl");
+        resource.addProperty(schemaUrlProperty, scopeSpans.getSchemaUrl());
+
+        Property scopeProperty = model.createProperty(ontoUri, "scope");
+        Resource instrumentationScopeResource = (new InstrumentationScopeConverter(model, scopeSpans.getScope())).getConvertedResource();
+        resource.addProperty(scopeProperty, instrumentationScopeResource);
+
+        Property spanProperty = model.createProperty(ontoUri, "span");
+
+
+        return resource;
     }
 }
