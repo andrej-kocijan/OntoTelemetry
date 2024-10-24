51d55b64bce7e13e2b6692cdeb659fd97a836b3b
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-10-24 16:15:31 +0200

    Save built models to the db

--- src/main/java/si/fri/liis/Services/TracesService.java
+++ src/main/java/si/fri/liis/Services/TracesService.java
@@ -1,7 +1,9 @@
 package si.fri.liis.Services;
 
 import io.opentelemetry.proto.trace.v1.TracesData;
+import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdfconnection.RDFConnectionFuseki;
+import org.apache.jena.system.Txn;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,12 +25,21 @@ public class TracesService {
 
     public void HandleTrace(TracesData tracesData) {
 
+        Model model;
+
         try (RDFConnectionFuseki conn = connFusekiFactory.createQueryConnection()) {
             TraceConverter tc = new TraceConverter(tracesData, conn);
-            tc.getConvertedModel().write(System.out, "TURTLE");
-            System.out.println("------------------------------------------------------");
+            model = tc.getConvertedModel();
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+            return;
+        }
+
+        try (RDFConnectionFuseki conn = connFusekiFactory.createLoadConnection()) {
+            Txn.executeWrite(conn, () -> conn.load(model));
         } catch (Exception e) {
             logger.error(e.getMessage());
         }
+
     }
 }
