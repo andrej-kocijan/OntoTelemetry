9f46dd0b92b82cc30900e8de71969b28a53880ce
Andrej Kocijan <kocijan.andrej@gmail.com>
2024-10-31 13:39:16 +0100

    Added existing resource check

--- src/main/java/si/fri/liis/Converters/Common/ResourceConverter.java
+++ src/main/java/si/fri/liis/Converters/Common/ResourceConverter.java
@@ -1,13 +1,17 @@
 package si.fri.liis.Converters.Common;
 
 import io.opentelemetry.proto.common.v1.KeyValue;
+import org.apache.jena.query.Query;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdfconnection.RDFConnectionFuseki;
+import org.apache.jena.system.Txn;
 import org.apache.jena.vocabulary.RDF;
+import si.fri.liis.Helpers.QueryHelpers;
 
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ResourceConverter extends CommonConverter<io.opentelemetry.proto.resource.v1.Resource> {
 
@@ -17,6 +21,21 @@ public class ResourceConverter extends CommonConverter<io.opentelemetry.proto.re
 
     @Override
     protected void convertToModel() {
+
+        String serviceName = this.source
+                .getAttributesList()
+                .stream()
+                .filter(a -> a.getKey().equals("service.name") && a.getValue().hasStringValue())
+                .map(a -> a.getValue().getStringValue())
+                .findFirst()
+                .orElse( "noServiceName");
+
+        Resource existing = checkForExisting(serviceName);
+
+        if (existing != null) {
+            this.resource = existing;
+            return;
+        }
 
         this.resource = this.model.createResource(ontoUri + "resource" + UUID.randomUUID());
 
@@ -28,13 +47,6 @@ public class ResourceConverter extends CommonConverter<io.opentelemetry.proto.re
         resource.addProperty(RDF.type, resourceProperty);
         resource.addLiteral(droppedAttributesCountProperty, this.source.getDroppedAttributesCount());
 
-        String serviceName = this.source
-                .getAttributesList()
-                .stream()
-                .filter(a -> a.getKey().equals("service.name") && a.getValue().hasStringValue())
-                .map(a -> a.getValue().getStringValue())
-                .findFirst()
-                .orElse( "noServiceName");
         resource.addLiteral(serviceNameProperty, serviceName);
 
         for (KeyValue attribute : this.source.getAttributesList())
@@ -45,5 +57,33 @@ public class ResourceConverter extends CommonConverter<io.opentelemetry.proto.re
             Resource attributeResource = (new KeyValueConverter(model, attribute)).getConvertedResource();
             resource.addProperty(attributeProperty, attributeResource);
         }
+    }
+
+    private Resource checkForExisting(String serviceName) {
+
+        if(serviceName.equals("noServiceName"))
+            return null;
+
+        AtomicReference<Resource> resource = new AtomicReference<>(null);
+
+        Query q = QueryHelpers.createQuery(String.format("""
+                SELECT ?resource
+                WHERE {
+                    ?resource a :Resource ;
+                        :serviceName "%s" .
+                }
+                LIMIT 1
+                """, serviceName));
+
+        try {
+            Txn.executeRead(conn, () -> conn.querySelect(q, (result) -> {
+                Resource r = result.getResource("resource");
+                resource.set(r);
+            }));
+        } catch (Exception e) {
+            System.err.println("Error while querying for existing resource: " + e.getMessage());
+        }
+
+        return resource.get();
     }
 }
